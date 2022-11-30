26fa91d74e0ac7b538fdfbfb907280df5c2c151b
Rupesh0913 <63779583+Rupesh0913@users.noreply.github.com>
2022-11-30 10:30:14 +0530

    Create Palindrome.js

---
+++ Palindrome.js
@@ -0,0 +1,20 @@
+  let str="madam";
+  let N=str.length;
+
+  let isPalindrome=true;
+  
+  for(let i=0;i<N/2;i++)
+  {
+      if(str[i]==str[N-i])
+      continue;
+      else
+      {
+          isPalindrome=false;
+          break;
+      }
+  }
+  if(isPalindrome)
+  console.log("Yes");
+  else
+  console.log("No");
+

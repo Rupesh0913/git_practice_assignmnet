d87a2208022751b54c4c1d9018b74f45a95f51a5
Rupesh0913 <63779583+Rupesh0913@users.noreply.github.com>
2022-11-30 10:29:07 +0530

    Create prime.js

---
+++ prime.js
@@ -0,0 +1,25 @@
+
+   let num =4;
+
+    let count=0;
+    let printed=false;
+   if(num==1||num==0){
+   console.log("Yes");
+   printed=true;
+   }
+   else
+        for(let i=2;i<=num/2;i++)
+        if(i%num==0){
+            count++;
+            break;
+        }
+    
+    if(printed==false)
+    {
+    if(count==0)
+        console.log("Yes");
+    else
+        console.log("No");
+    }
+    
+
